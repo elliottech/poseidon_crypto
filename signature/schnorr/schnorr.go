59932f18790a283a13d85e8f0f4a001296917671
bbugrul <burak@lighter.xyz>
2024-10-22 14:09:49 +0100

    more info

--- signature/schnorr/schnorr.go
+++ signature/schnorr/schnorr.go
@@ -130,13 +130,20 @@ func Validate(pubKey, hashedMsg, sig []byte) error {
 }
 
 func IsSchnorrSignatureValid(pubKey, hashedMsg *gFp5.Element, sig Signature) bool {
+	start := time.Now()
 	pubKeyWs, ok := curve.DecodeFp5AsWeierstrass(*pubKey)
 	if !ok {
 		return false
 	}
+	since := time.Since(start)
+	fmt.Printf("`pubKeyWs calculation` took %s nanosecond\n", field.FormatWithUnderscores(since.Nanoseconds()))
 
+	start = time.Now()
 	rV := curve.MulAdd2(curve.GENERATOR_WEIERSTRASS, pubKeyWs, sig.S, sig.E).Encode() // r_v = s*G + e*pk
+	since = time.Since(start)
+	fmt.Printf("`rV calculation` took %s nanosecond\n", field.FormatWithUnderscores(since.Nanoseconds()))
 
+	start = time.Now()
 	preImage := make([]g.Element, 5+5)
 	for i, elem := range rV.ToBasefieldArray() {
 		preImage[i] = elem
@@ -144,7 +151,18 @@ func IsSchnorrSignatureValid(pubKey, hashedMsg *gFp5.Element, sig Signature) boo
 	for i, elem := range hashedMsg.ToBasefieldArray() {
 		preImage[i+5] = elem
 	}
-	eV := curve.FromGfp5(p2.HashToQuinticExtension(preImage))
+	since = time.Since(start)
+	fmt.Printf("`preImage calculation` took %s nanosecond\n", field.FormatWithUnderscores(since.Nanoseconds()))
 
-	return eV.Equals(&sig.E) // e_v == e
+	start = time.Now()
+	eV := curve.FromGfp5(p2.HashToQuinticExtension(preImage))
+	since = time.Since(start)
+	fmt.Printf("`eV calculation` took %s nanosecond\n", field.FormatWithUnderscores(since.Nanoseconds()))
+
+	start = time.Now()
+	result := eV.Equals(&sig.E) // e_v == e
+	since = time.Since(start)
+	fmt.Printf("`eV.Equals(&sig.E)` took %s nanosecond\n", field.FormatWithUnderscores(since.Nanoseconds()))
+
+	return result
 }

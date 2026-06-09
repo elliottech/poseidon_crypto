4443756f696f351688ec7f9b9a2081ec755db7a3
Mychecksdead <mert.koksal.026@gmail.com>
2026-06-09 22:38:04 +0100

    wow

--- field/goldilocks/goldilocks_plonky2.go
+++ field/goldilocks/goldilocks_plonky2.go
@@ -55,8 +55,8 @@ func (z GoldilocksField) ToCanonicalUint64() uint64 {
 // lhs, rhs in non-canonical form
 func AddF(lhs, rhs GoldilocksField) GoldilocksField {
 	sum, over := bits.Add64(uint64(lhs), uint64(rhs), 0)
-	sum, over = bits.Add64(sum, over*EPSILON, 0)
-	sum += EPSILON * over // this can't overflow
+	sum, over = bits.Add64(sum, (EPSILON & -over), 0)
+	sum += (EPSILON & -over) // this can't overflow
 
 	return GoldilocksField(sum)
 }
@@ -65,7 +65,7 @@ func AddF(lhs, rhs GoldilocksField) GoldilocksField {
 func AddCanonicalUint64(lhs GoldilocksField, rhs uint64) GoldilocksField {
 	sum, over := bits.Add64(uint64(lhs), rhs, 0)
 	// if overflowed, sum := lhs + rhs - 2^64 => sum + EPSILON = lhs + rhs - 2^64 + 2^32 -1 = lhs + rhs - ORDER < ORDER + 2^64 - ORDER = 2^64, so there is no overflow in this case.
-	return GoldilocksField(sum + over*EPSILON)
+	return GoldilocksField(sum + (EPSILON & -over))
 }
 
 func DoubleF(lhs GoldilocksField) GoldilocksField {
@@ -75,8 +75,8 @@ func DoubleF(lhs GoldilocksField) GoldilocksField {
 // lhs, rhs in non-canonical form
 func SubF(lhs, rhs GoldilocksField) GoldilocksField {
 	diff, borrow := bits.Sub64(uint64(lhs), uint64(rhs), 0)
-	diff, borrow = bits.Sub64(diff, borrow*EPSILON, 0)
-	diff -= EPSILON * borrow // this can't underflow
+	diff, borrow = bits.Sub64(diff, (EPSILON & -borrow), 0)
+	diff -= (EPSILON & -borrow) // this can't underflow
 
 	return GoldilocksField(diff)
 }
@@ -89,12 +89,12 @@ func MulF(lhs, rhs GoldilocksField) GoldilocksField {
 	x_hi_lo := x_hi & EPSILON
 
 	t0, borrow := bits.Sub64(x_lo, x_hi_hi, 0)
-	t0 -= EPSILON * borrow
+	t0 -= (EPSILON & -borrow)
 
 	t1 := x_hi_lo * EPSILON
 
 	sum, over := bits.Add64(t0, t1, 0)
-	t2 := sum + EPSILON*over
+	t2 := sum + (EPSILON & -over)
 	return GoldilocksField(t2)
 }
 
@@ -105,12 +105,12 @@ func SquareF(x GoldilocksField) GoldilocksField {
 	x_hi_lo := x_hi & EPSILON
 
 	t0, borrow := bits.Sub64(x_lo, x_hi_hi, 0)
-	t0 -= EPSILON * borrow
+	t0 -= (EPSILON & -borrow)
 
 	t1 := x_hi_lo * EPSILON
 
 	sum, over := bits.Add64(t0, t1, 0)
-	t2 := sum + EPSILON*over
+	t2 := sum + (EPSILON & -over)
 	return GoldilocksField(t2)
 }
 
@@ -121,9 +121,9 @@ func MulAccF(self, x, y GoldilocksField) GoldilocksField {
 	hi += c
 
 	t0, borrow := bits.Sub64(lo, hi>>32, 0)
-	t0 -= EPSILON * borrow
+	t0 -= (EPSILON & -borrow)
 	resWrapped, c := bits.Add64(t0, (hi&EPSILON)*EPSILON, 0)
-	return GoldilocksField(resWrapped + EPSILON*c)
+	return GoldilocksField(resWrapped + (EPSILON & -c))
 }
 
 func ExpPowerOf2(x GoldilocksField, n uint) GoldilocksField {
@@ -189,7 +189,7 @@ func Reduce96Bit(x UInt128) GoldilocksField {
 	t1 := x.Hi * EPSILON
 	resWrapped, carry := bits.Add64(x.Lo, t1, 0)
 
-	return GoldilocksField(resWrapped) + GoldilocksField(carry*EPSILON)
+	return GoldilocksField(resWrapped) + GoldilocksField((EPSILON & -carry))
 }
 
 func Reduce128Bit(x UInt128) GoldilocksField {
@@ -197,12 +197,12 @@ func Reduce128Bit(x UInt128) GoldilocksField {
 	x_hi_lo := x.Hi & EPSILON
 
 	t0, borrow := bits.Sub64(x.Lo, x_hi_hi, 0)
-	t0 -= EPSILON * borrow
+	t0 -= (EPSILON & -borrow)
 
 	t1 := x_hi_lo * EPSILON
 
 	resWrapped, carry := bits.Add64(t0, t1, 0)
-	t2 := resWrapped + EPSILON*carry
+	t2 := resWrapped + (EPSILON & -carry)
 	return GoldilocksField(t2)
 }
 
